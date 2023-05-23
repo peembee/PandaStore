57dc819a2bc8cb77b7587182eec8a08b3c1e6c22
pmb <p.boukhedimi@hotmail.com>
2023-05-23 13:15:36 +0200

    fixing inputs in edit profile and register-page

--- PandaStore/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ PandaStore/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -3,6 +3,7 @@
 #nullable disable
 
 using System;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
@@ -43,6 +44,9 @@ namespace PandaStore.Areas.Identity.Pages.Account.Manage
         ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
         ///     directly from your code. This API may change or be removed in future releases.
         /// </summary>
+        /// 
+
+
         [BindProperty]
         public InputModel Input { get; set; }
 
@@ -56,6 +60,31 @@ namespace PandaStore.Areas.Identity.Pages.Account.Manage
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
+            [Required]
+            [StringLength(50)]
+            [DisplayName("First name")]
+            public string FirstName { get; set; }
+
+            [Required]
+            [StringLength(50)]
+            [DisplayName("Last name")]
+            public string LastName { get; set; }
+            [Required]
+            [StringLength(70)]
+            public string Address { get; set; }
+
+            [Required]
+            [StringLength(5)]
+            public string ZipCode { get; set; }
+
+            [Required]
+            [StringLength(50)]
+            public string City { get; set; }
+            [Required]
+            [StringLength(20)]
+
+
+
             [Phone]
             [Display(Name = "Phone number")]
             public string PhoneNumber { get; set; }
@@ -66,11 +95,17 @@ namespace PandaStore.Areas.Identity.Pages.Account.Manage
             var userName = await _userManager.GetUserNameAsync(user);
             var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
 
+
             Username = userName;
 
             Input = new InputModel
             {
-                PhoneNumber = phoneNumber
+                PhoneNumber = phoneNumber,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                City = user.City,
+                Address = user.Adress,
+                ZipCode = user.ZipCode
             };
         }
 
@@ -111,7 +146,24 @@ namespace PandaStore.Areas.Identity.Pages.Account.Manage
                 }
             }
 
+            user.FirstName = Input.FirstName.Trim();
+            user.FirstName = user.FirstName = char.ToUpper(user.FirstName[0]) + user.FirstName.Substring(1);
+
+            user.LastName = Input.LastName.Trim();
+            user.LastName = user.LastName = char.ToUpper(user.LastName[0]) + user.LastName.Substring(1);
+
+            user.Adress = Input.Address.Trim();
+            user.Adress = user.Adress = char.ToUpper(user.Adress[0]) + user.Adress.Substring(1);
+
+            user.ZipCode = Input.ZipCode.Trim();
+
+            user.City = Input.City.Trim();
+            user.City = user.City = char.ToUpper(user.City[0]) + user.City.Substring(1);
+
+            await _userManager.UpdateAsync(user);
+
             await _signInManager.RefreshSignInAsync(user);
+
             StatusMessage = "Your profile has been updated";
             return RedirectToPage();
         }
