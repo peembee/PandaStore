2ba8724d070dc455f221dfd0a73810dc194e25fe
Kristina Eriksson <wetonen@gmail.com>
2023-05-26 17:47:00 +0200

    added a electronic page with little design

---
+++ PandaStore/Controllers/ElectronicsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PandaStore.Controllers
+{
+    public class ElectronicsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
