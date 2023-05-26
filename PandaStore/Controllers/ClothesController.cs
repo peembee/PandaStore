1bac99df4a3748b6776b6fc4e02b7138dc8e6e31
Engwalls <erik.97@hotmail.com>
2023-05-26 14:04:33 +0200

    Fixed a clothingpage with products. Will need more design.

---
+++ PandaStore/Controllers/ClothesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PandaStore.Data;
+using PandaStore.Models;
+using PandaStore.Repository;
+using System.Diagnostics;
+
+namespace PandaStore.Controllers
+{
+    public class ClothesController : Controller
+    {
+        public IActionResult ClothesProducts()
+        {
+            return View();
+        }
+    }
+}
