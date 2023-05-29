85c53f92505701be7dbe17399897aadb7e66401d
silvergripen <57497725+silvergripen@users.noreply.github.com>
2023-05-29 16:51:44 +0200

    Added Food and alcohol controllers and views. Updated navbar with new categories Made it so they all use @item.ImgUrl just to future proof it.

---
+++ PandaStore/Controllers/AlcoholController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PandaStore.Data;
+
+namespace PandaStore.Controllers
+{
+    public class AlcoholController : Controller
+    {
+        private readonly PandaStoreContext _context;
+        public AlcoholController(PandaStoreContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Details()
+        {
+            var products = await _context.Products
+                .Include(c => c.Campaigns)
+                .Include(p => p.Categorys)
+                .Where(p => p.FK_CategoryID == 7)
+                .ToListAsync();
+
+            return View(products);
+        }
+    }
+}
+
