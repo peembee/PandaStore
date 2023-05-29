c44bed6174a3445de388440c3b9055dcb7f9e2b2
Kristina Eriksson <wetonen@gmail.com>
2023-05-29 22:01:41 +0200

    change the stringlenght to ImgUrl and fixed the form for products

---
+++ PandaStore/Data/Migrations/20230529195855_change ImgUrl stringlenght.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PandaStore.Migrations
+{
+    /// <inheritdoc />
+    public partial class changeImgUrlstringlenght : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ImgUrl",
+                table: "Products",
+                type: "nvarchar(80)",
+                maxLength: 80,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ImgUrl",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(80)",
+                oldMaxLength: 80,
+                oldNullable: true);
+        }
+    }
+}
