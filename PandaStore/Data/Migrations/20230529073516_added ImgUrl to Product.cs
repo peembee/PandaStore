0ea5cd478622e6771001a3024ed01e7ca799a808
Kristina Eriksson <wetonen@gmail.com>
2023-05-29 09:45:21 +0200

    added a new prop to Product model, ImgUrl

---
+++ PandaStore/Data/Migrations/20230529073516_added ImgUrl to Product.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PandaStore.Migrations
+{
+    /// <inheritdoc />
+    public partial class addedImgUrltoProduct : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImgUrl",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ProductName",
+                table: "CustomerProducts",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImgUrl",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "ProductName",
+                table: "CustomerProducts");
+
+            
+        }
+    }
+}
